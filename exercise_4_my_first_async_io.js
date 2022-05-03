c7b9a4a6cf0ce94e12cc2123fab70e971f635f1a
Igor Wnek <igowne@gmail.com>
2022-05-03 11:16:07 +0200

    My First Async I/O - exercise 5 - suggested solution

--- exercise_4_my_first_async_io.js
+++ exercise_4_my_first_async_io.js
@@ -1,10 +1,14 @@
 'use strict'
 const fs = require('fs')
+const file = process.argv[2]
 
 function countNewLinesInFile(error, fileContentsString) {
+    if (error) {
+        return console.log(error)
+    }
     const newLinesNumber = fileContentsString.split('\n').length - 1
     console.log(newLinesNumber)
 }
 
-fs.readFile(process.argv[2], 'utf8', countNewLinesInFile)
+fs.readFile(file, 'utf8', countNewLinesInFile)
 
