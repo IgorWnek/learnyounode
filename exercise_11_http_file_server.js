e9501b897d3dfd172832849f99c6fa781c5a3a50
Igor Wnek <igowne@gmail.com>
2022-05-08 11:40:41 +0200

    HTTP File Server - exercise 11

---
+++ exercise_11_http_file_server.js
@@ -0,0 +1,13 @@
+'use strict'
+const http = require('http')
+const fs = require('fs')
+
+const port = Number(process.argv[2])
+const path = process.argv[3]
+
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' })
+
+    fs.createReadStream(path).pipe(res)
+})
+server.listen(port)
